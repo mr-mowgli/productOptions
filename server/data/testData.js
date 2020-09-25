ab2ba30bf750ccc592101ca6b772725aa7fcd043
Aziz Bouland <q8boland@gmail.com>
2020-09-25 05:57:23 +0300

    initialed MySQL with Sequelize and basic endpoint API with test data

---
+++ server/data/testData.js
@@ -0,0 +1,14 @@
+var data = [
+  {
+    productID: '1',
+    name: "Men's Standard Fit Hoodie Sweatshirt - Goodfellow & Co™",
+    price: 15.3,
+    reviews: 4.75,
+    reviewCount: 120,
+    sizes: ["s", "m", "l", "xl", "xxl", "xxxl"],
+    colors: ["red", "navy", "black", "smoke"]
+}
+]
+
+// {price: 13.5,  colors: [foo, bar, ...], sizes: [S, M, L, …], reviews: 4.5, reviewCount: 18 }
+module.exports = data;
