c8bb60e99bd0fe6dca4af7760323d2428692711b
Aziz Bouland <q8boland@gmail.com>
2020-09-27 02:22:53 +0300

    database populates with correct relationships and without duplicating stores or products

--- server/database/resetDB.sql
+++ server/database/resetDB.sql
@@ -1,13 +1,8 @@
 -- to wipe data:
-DROP DATABASE if EXISTS options;
-create DATABASE options;
-USE options;
-
+-- TRUNCATE TABLE stocks;TRUNCATE TABLE products;TRUNCATE TABLE stores;
 
 -- to select:
-select * from stocks;
-select * from products;
-select * from stores;
+-- select * from stocks;select * from products;select * from stores;
 
 -- to run script run:
 -- mysql -u root < database/resetDB.sql
