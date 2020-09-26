36652a3233af6ef091db96d09bf788619eefa375
Aziz Bouland <q8boland@gmail.com>
2020-09-26 06:10:39 +0300

    Working sequelize DB, refactored to be more modular

--- server/database/index.js
+++ server/database/index.js
@@ -6,45 +6,6 @@ const db = new Sequelize(
     dialect: 'mysql'
   });
 
-const Product = db.define('Product', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  price: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  reviews: {
-   type: DataTypes.INTEGER,
-    defaultValue: 0
-  },
-  reviewCount: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0
-  }
-})
-
-const Stock = db.define('Stock', {
-  color: {
-    type: DataTypes.STRING
-  },
-  size: {
-    type: DataTypes.STRING
-  },
-  qty: {
-   type: DataTypes.INTEGER,
-    defaultValue: 0
-  }
-});
-
-Product.hasMany(Stock);
-// Stock.belongsTo(Product); need this one??
-
-Product.sync();
-Stock.sync();
-
-
 try {
   db.authenticate();
   console.log('db connection successful!');
@@ -52,3 +13,5 @@ try {
   console.error('Unable to connect to DB', error);
 }
 
+
+module.exports = db;
---
+++ server/database/resetDB.sql
@@ -0,0 +1,6 @@
+DROP DATABASE if EXISTS options;
+create DATABASE options;
+USE options;
+
+-- to run script run:
+-- mysql -u root < database/resetDB.sql
