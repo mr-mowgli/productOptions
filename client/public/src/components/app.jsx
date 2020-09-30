0185e2487972ed6267bbc93dc7a93452809d1e1e
Aziz Bouland <q8boland@gmail.com>
2020-09-30 21:40:51 +0300

    fixed based on PR review

--- client/public/src/components/app.jsx
+++ client/public/src/components/app.jsx
@@ -9,34 +9,34 @@ const App = () => {
   const [productId, setProductId] = useState(1);
   const [product, setProduct] = useState({});
   const [storeId, setStoreId] = useState(1);
-  const [stores, setStore] = useState([]);
+  const [stores, setStores] = useState([]);
   const [stock, setStock] = useState([]);
   const [colors, setColors] = useState([]);
   const [sizes, setSizes] = useState([]);
 
   // get the products general details such as price, name and reviews
   const getProduct = async (productId) => {
-    const response = await axios.get(`http://localhost:3000/products/${productId}`);
-    const productData = await response.data[0];
+    const response = await axios.get(`/products/${productId}`);
+    const productData = response.data[0];
     setProduct(productData);
   }
 
   // get a list of all the stores (names and ids)
   const getStores = async () => {
-    const response = await axios.get(`http://localhost:3000/stores`);
-    const storesData = await response.data;
-    setStore(storesData);
+    const response = await axios.get(`/stores`);
+    const storesData = response.data;
+    setStores(storesData);
   }
 
   // get all available stock for the selected item in the selected store
   const getStock = async (productId, stores) => {
-    const response = await axios.get(`http://localhost:3000/stock/${productId}`);
-    const stockData = await response.data;
+    const response = await axios.get(`/stock/${productId}`);
+    const stockData = response.data;
     // find the store that matches the selected store state
-    const selectedStore = await stores.find(store => store.id === storeId);
+    const selectedStore = stores.find(store => store.id === storeId);
     if (selectedStore !== undefined) {
       // filter to only have the selected store's stock for the selected product
-      const productStock = await stockData.filter(item => item.location === selectedStore.location);
+      const productStock = stockData.filter(item => item.location === selectedStore.location);
       setStock(productStock);
       // extract colors and sizes from the product stock
       var colors = {};
