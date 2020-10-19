2226cf423b13f4706028f7caffdbc5067c8bc3d5
Brian Hauck <brihauck4@gmail.com>
2020-10-19 16:10:12 -0700

    data generating file with approx 30 million records

--- server/sdc/dataGeneration.js
+++ server/sdc/dataGeneration.js
@@ -1,5 +1,5 @@
 const faker = require('faker');
-const {Product, Stock, Store} = require('./models.js');
+const fs = require('fs');
 
 // 5 store locations
 const stores = ['boulder', 'longmont', 'superior', 'westminister', 'aurora'];
@@ -23,44 +23,47 @@ const random = function(max, min) {
   return Math.floor(Math.random() * (max - min) + min)
 };
 
+const reviewStat = function() {
+  return Math.random() * 5
+};
+
+const reviewCount = function() {
+  return Math.floor(Math.random() * 90)
+};
+
 // generate all data for DB
-let generateProducts = function(quantity) {
-  // first create the 5 store locations with ids 1 through 5
-  for (var s = 1; s <= stores.length; s++) {
-    let curStore = {
-      id: s,
-      location: stores[s]
-    }
-    Store.create(curStore)
-  }
+const generateProducts = async function(quantity) {
+  var dataOut = fs.createWriteStream('data.txt');
   // create all the products and 1 - 3 stock per product
   for (var p = 1; p <= quantity; p++) {
     let curProduct = {
       id: p,
       name: faker.commerce.product(),
       price: faker.finance.amount(),
-      reviews: faker.something(),
-      reviewCount: faker.something()
+      reviews: reviewStat(),
+      reviewCount: reviewCount()
     };
 
-    Product.create(curProduct);
-
     let stockCount = random(3, 1);
-    for (var k = 0; k <= stockCount; k++) {
-      let storeid = random(stores.length, 1);
+    for (var k = 1; k <= stockCount; k++) {
+      let storeindex = random(stores.length);
       let colorindex = random(colors.length);
       let sizeindex = random(sizes.length);
       let quantity = random(9, 1);
       let curStock = {
         id: k,
-        ProductId: p,
-        StoreId: storeid,
+        Product: curProduct,
+        Store: storeindex,
         color: colors[colorindex],
         size: sizes[sizeindex],
         qty:quantity
       };
-      Stock.create(curStock)
+      let loadRecord = JSON.stringify(curStock)
+      if (!dataOut.write(loadRecord)) {
+        await new Promise(resolve => dataOut.once('drain', resolve))
+      }
     }
-
   }
-}
+  dataOut.end()
+};
+generateProducts(10000000);
